948984a57f37465305111b590d30399ef1432118
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-02-18 14:00:53 +0100

    Progress

--- src/main.cpp
+++ src/main.cpp
@@ -40,33 +40,40 @@ class Pet
         int hunger;
 };
 
+	class Polynomial
+	{
+	public:
+		Polynomial() : a_(0), b_(0), c_(0), d_(0), p_scale_(1.0) {}
+		Polynomial(double a, double b, double c, double d, double p_scale = 1) : a_(a), b_(b), c_(c), d_(d), p_scale_(p_scale) {}
+		void Set(double a, double b, double c, double d, double p_scale = 1);
+		void SetA(double a) { a_ = a; }
+		void SetB(double b) { b_ = b; }
+		void SetC(double c) { c_ = c; }
+		void SetD(double d) { d_ = d; }
+		double GetA() { return a_; }
+		double GetB() { return b_; }
+		double GetC() { return c_; }
+		double GetD() { return d_; }
+		double GetPscale() { return p_scale_; }
+		double Evaluate(double s);
+		double EvaluatePrim(double s);
+		double EvaluatePrimPrim(double s);
+
+	private:
+		double a_;
+		double b_;
+		double c_;
+		double d_;
+		double p_scale_;
+	};
+
 namespace py = pybind11;
+using namespace roadmanager;
 
 PYBIND11_MODULE(cmake_example, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
 
-        .. currentmodule:: cmake_example
-
-        .. autosummary::
-           :toctree: _generate
-
-           add
-           subtract
-    )pbdoc";
-
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("myesmini", &myesmini, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
+    m.def("add", &add);
+    m.def("myesmini", &myesmini);
 
     // bindings to Pet class
     py::class_<Pet>(m, "Pet")
@@ -75,6 +82,53 @@ PYBIND11_MODULE(cmake_example, m) {
         .def("get_hunger", &Pet::get_hunger)
         .def("get_name", &Pet::get_name);
 
+    // bindings to Polynomial class
+    py::class_<roadmanager::Polynomial>(m, "Polynomial")
+        .def(py::init<>())
+        .def(py::init<double , double , double , double , double>())
+        .def("Set", &roadmanager::Polynomial::Set)
+        .def("SetA", &roadmanager::Polynomial::SetA)
+        .def("SetB", &roadmanager::Polynomial::SetB)
+        .def("SetC", &roadmanager::Polynomial::SetC)
+        .def("SetD", &roadmanager::Polynomial::SetD)
+        .def("GetA", &roadmanager::Polynomial::GetA)
+        .def("GetB", &roadmanager::Polynomial::GetB)
+        .def("GetC", &roadmanager::Polynomial::GetC)
+        .def("GetD", &roadmanager::Polynomial::GetD)
+        .def("GetPscale", &roadmanager::Polynomial::GetPscale)
+        .def("Evaluate", &roadmanager::Polynomial::Evaluate)
+        .def("EvaluatePrim", &roadmanager::Polynomial::EvaluatePrim)
+        .def("EvaluatePrimPrim", &roadmanager::Polynomial::EvaluatePrimPrim)
+        ;
+
+    // bindings to OSIPoints class
+    py::class_<roadmanager::OSIPoints>(m, "OSIPoints")
+        .def(py::init<>())
+        //.def(py::init<std::vector<roadmanager::OSIPoints::OSIPointStruct> >())
+        ;
+/*
+	class OSIPoints
+	{
+		public:
+			typedef struct
+			{
+				double s;
+				double x;
+				double y;
+				double z;
+				double h;
+			} OSIPointStruct;
+
+			OSIPoints() {}
+			OSIPoints(std::vector<OSIPointStruct> points) : point_(points) {}
+			void Set(std::vector<OSIPointStruct> points) { point_ = points; }
+			std::vector<OSIPointStruct>& GetPoints() {return point_;}
+			OSIPointStruct& GetPoint(int i);
+			double GetXfromIdx(int i);
+			double GetYfromIdx(int i);
+			double GetZfromIdx(int i);
+			int GetNumOfOSIPoints();
+*/
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
--- test.py
+++ test.py
@@ -8,3 +8,24 @@ print(my_dog.get_hunger())
 my_dog.go_for_a_walk()
 print(my_dog.get_hunger())
 
+my_pol = Polynomial(4,5,6,7,8)
+my_pol.Set(1,2,3,4,5)
+print(my_pol.GetA())
+print(my_pol.GetB())
+print(my_pol.GetC())
+print(my_pol.GetD())
+
+
+my_pol.SetA(14)
+my_pol.SetB(15)
+my_pol.SetC(16)
+my_pol.SetD(17)
+
+print(my_pol.GetA())
+print(my_pol.GetB())
+print(my_pol.GetC())
+print(my_pol.GetD())
+
+
+my_osi_point = OSIPoints()
+# my_osi_point = OSIPoints(["x",2,3,4,5])
