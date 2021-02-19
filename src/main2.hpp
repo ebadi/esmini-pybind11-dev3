1b369750b1efa913746b4735fa1f2492669a085f
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-02-19 11:16:29 +0100

    RosettaCommons binder

---
+++ src/main2.hpp
@@ -0,0 +1,9 @@
+
+//#include "esminiLib.hpp"
+#include "RoadManager.hpp"
+//#include "CommonMini.hpp"
+//#include "pugixml.hpp"
+
+/*
+/home/wave/binder/bin/binder --root-module esmini-pybinder11 --prefix /home/wave/repositories/esmini-pybind11/esmini/EnvironmentSimulator/Modules/RoadManager/ --bind roadmanager /home/wave/repositories/esmini-pybind11/src/main2.hpp  -- -std=c++11 -Iesmini/externals/pugixml/ -Iesmini/EnvironmentSimulator/Modules/CommonMini/ -Iesmini/EnvironmentSimulator/Libraries/esminiLib/ -Iesmini/EnvironmentSimulator/Modules/RoadManager -I/home/wave/repositories/esmini-pybind11/esmini/bin/  -DMY_PROJECT_DEFINE -DNDEBUG
+*/
