4833ed931cd1cb5eb6a81d979149d5072085bbfb
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-02-27 08:57:24 +0100

    cleaning up

--- run.sh
+++ run.sh
@@ -4,17 +4,23 @@
 # commonmini
 # ---------------------------
 
-# IPATHS=" -Iesmini/externals/pugixml/ -Iesmini/EnvironmentSimulator/Modules/CommonMini/ -Iesmini/EnvironmentSimulator/Modules/RoadManager   -Iesmini/EnvironmentSimulator/Modules/Controllers  -Iesmini/EnvironmentSimulator/Modules/PlayerBase -Iesmini/EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/  -Iesmini/EnvironmentSimulator/Modules/ScenarioEngine/OSCTypeDefs/ -Iesmini/EnvironmentSimulator/Modules/ViewerBase -Iesmini/EnvironmentSimulator/Libraries/esminiLib/  -I/home/wave/repositories/esmini-pybind11/esmini/bin/ "
+IPATHS=" -Iesmini/externals/pugixml/ -Iesmini/EnvironmentSimulator/Modules/CommonMini/ -Iesmini/EnvironmentSimulator/Modules/RoadManager   -Iesmini/EnvironmentSimulator/Modules/Controllers  -Iesmini/EnvironmentSimulator/Modules/PlayerBase -Iesmini/EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/  -Iesmini/EnvironmentSimulator/Modules/ScenarioEngine/OSCTypeDefs/ -Iesmini/EnvironmentSimulator/Modules/ViewerBase -Iesmini/EnvironmentSimulator/Libraries/esminiLib/  -I/home/wave/repositories/esmini-pybind11/esmini/bin/ "
 # MYMODULE=roadmanager
 # rm -rf $PWD/src/$MYMODULE ;  mkdir  $PWD/src/$MYMODULE
 # /home/wave/binder/bin/binder --root-module py$MYMODULE --prefix $PWD/src/$MYMODULE --bind $MYMODULE  --single-file   --config $MYMODULE.cfg  $PWD/src/$MYMODULE.hpp  -- -std=c++11 -DNDEBUG $IPATHS
 # pip3 install . ;
-# python3 -c "import pyroadmanager; print(dir());"
+# python3 -c "import pyroadmanager; print(dir(pyroadmanager));"
 
 
-MYMODULE=scenarioengine
+#MYMODULE=scenarioengine
+#rm -rf $PWD/src/$MYMODULE ;  mkdir  $PWD/src/$MYMODULE
+#/home/wave/binder/bin/binder --root-module py$MYMODULE --prefix $PWD/src/$MYMODULE --bind $MYMODULE  --single-file  --config $MYMODULE.cfg  $PWD/src/$MYMODULE.hpp  -- -std=c++11 -DNDEBUG $IPATHS
+#pip3 install . ;
+#python3 -c "import pyscenarioengine; print(dir(pyscenarioengine));"
+
+
+MYMODULE=playerbase
 rm -rf $PWD/src/$MYMODULE ;  mkdir  $PWD/src/$MYMODULE
-/home/wave/binder/bin/binder --root-module py$MYMODULE --prefix $PWD/src/$MYMODULE --bind $MYMODULE  --single-file  --config $MYMODULE.cfg  $PWD/src/$MYMODULE.hpp  -- -std=c++11 -DNDEBUG $IPATHS
+/home/wave/binder/bin/binder --root-module py$MYMODULE --prefix $PWD/src/$MYMODULE --bind $MYMODULE  --single-file  --config $MYMODULE.cfg  $PWD/src/esmini$MYMODULE.hpp  -- -std=c++11 -DNDEBUG $IPATHS
 pip3 install . ;
-python3 -c "import pyscenarioengine; print(dir());"
-
+python3 -c "import pyplayerbase; print(dir(pyplayerbase));"
