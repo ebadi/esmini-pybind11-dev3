e99d089fba0a517c768083c6ab92e0cd33b4d68e
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-04-07 14:10:49 +0200

    debug build

--- build_esmini.sh
+++ build_esmini.sh
@@ -4,8 +4,8 @@ git clone https://github.com/ebadi/esmini.git
 cd esmini
 mkdir build
 cd build
-cmake ../ -DUSE_OSG=true -DCMAKE_BUILD_TYPE=Release
-cmake --build . --config Release --target install
+cmake ../ -DUSE_OSG=true -DCMAKE_BUILD_TYPE=Debug
+cmake --build . --config Debug --target install
 cd ../
 cd ../
 
