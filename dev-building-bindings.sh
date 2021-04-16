22bf988612628a567ccb9c213122f3e86851933e
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-04-16 08:43:02 +0200

    avoid pip uninstall error

--- dev-building-bindings.sh
+++ dev-building-bindings.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pip3 uninstall cmake_example -y
 ./build_esmini.sh
 
 IPATHS=" -Iesmini/externals/pugixml/ -Iesmini/EnvironmentSimulator/Modules/CommonMini/ -Iesmini/EnvironmentSimulator/Modules/RoadManager -Iesmini/EnvironmentSimulator/Modules/Controllers  -Iesmini/EnvironmentSimulator/Modules/PlayerBase -Iesmini/EnvironmentSimulator/Modules/ScenarioEngine/SourceFiles/  -Iesmini/EnvironmentSimulator/Modules/ScenarioEngine/OSCTypeDefs/ -Iesmini/EnvironmentSimulator/Modules/ViewerBase -Iesmini/EnvironmentSimulator/Libraries/esminiLib/  -Iesmini/EnvironmentSimulator/Libraries/esminiRMLib/ -Iesmini/bin/ -Iesmini/externals/OpenSceneGraph/linux/include/"
